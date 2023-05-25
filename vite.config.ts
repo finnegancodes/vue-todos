fcb28ceddf28867c92f2d5c8b40b082f8842450d
DeepPurple101 <adam.oravec0@gmail.com>
2023-05-25 20:50:51 +0200

    fixed assets not loading

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vue-todos/",
+  base: "/vue-todos/dist/",
   plugins: [vue()],
   resolve: {
     alias: {
